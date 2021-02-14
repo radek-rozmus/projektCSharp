f7502f3c76a7d81d856dde881e555daf18b378f5
Radosław Rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-02-14 14:12:48 +0100

    v0.2 Podpięto bazę danych do programu, oraz utworzono panele logowania i rejestracji

---
+++ BibliotekaWPF/LogInPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BibliotekaWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LogInPage.xaml
+    /// </summary>
+    public partial class LogInPage : Page
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
