bb4f34067110293a6bc8e5aa6a7bbdde1a0d5eaa
Radosław Rozmus <radoslaw.rozmus@microsoft.wsei.edu.pl>
2021-02-21 23:19:58 +0100

    Refaktoryzacja - etap 2: Dodanie kompletnej dokumentacji XML dla klasy ReturnBookWindow.

--- BibliotekaWPF/ReturnBookWindow.xaml.cs
+++ BibliotekaWPF/ReturnBookWindow.xaml.cs
@@ -7,9 +7,19 @@ namespace BibliotekaWPF
 {
     /// <summary>
     /// Logika interakcji dla klasy ReturnBookWindow.xaml
+    /// Komponent ReturnBookWindow to 2 z 2 okien dostępnych dla użytkownika w aplikacji Biblioteka.
     /// </summary>
     public partial class ReturnBookWindow : Window
     {
+
+        /// <summary>
+        /// Właściwość Mw jest ustawiana przez konstruktor klasy ApplicationPage na okno główne aplikacji.
+        /// Kontekst służący do odwoływania się do właściwości okna głównego.
+        /// Właściwość Login typu int reprezentuje identyfikator aktualnie zalogowanego do aplikacji czytelnika.
+        /// Właściwość ApplicationContext jest kontekst panelu użytkownika, dla którego wywołane zostało okno zwrotu książek.
+        /// W programie może istnieć wyłącznie jedna instancja Typu ReturnBookWindow jednocześnie.
+        /// </summary>
+
         MainWindow mw;
         ApplicationPage applicationContext;
         private int login;
@@ -26,6 +36,15 @@ namespace BibliotekaWPF
             ApplicationContext = (ApplicationPage)Mw.MainFrame.Content;
             User.Content = User.Content.ToString() + " " + Login;
         }
+
+        /// <summary>
+        /// Metoda GenerateReturnPage renderuje dla użytkownika panel z książkami wypożyczonymi przez niego,
+        /// które z pomocą tego panelu może oddać do biblioteki.
+        /// Lista książek jest tworzona na podstawie tabeli Wypożyczenia z podłączonej bazy danych.
+        /// Brane pod uwagę są takie wypożyczenia, które były wykonane przez zalogowanego użytkowanika i mają StatusWypozyczenia "AKTYWNE".
+        /// Każda następna wypożyczona książka pojawia się w tym panelu po ponownym jego wyrenderowaniu.
+        /// Każda zwrócona przez użytkownika książka pojawi się w panelu użytkownika kontekstowej aplikacji.
+        /// </summary>
 
         public void GenerateReturnPage()
         {
@@ -56,6 +75,13 @@ namespace BibliotekaWPF
             }
         }
 
+        /// <summary>
+        /// Metoda ReturnBookDecisionButton wywoływana jest w momencie naciśnięcia przez użytkownika przycisku ReturnBookDecisionButton.
+        /// Jeżeli użytkownik wybrał z listy książkę, którą zamierza zwrócić to dla danego wypożyczenia metoda zmienia StatusWypozyczenia na "ODDANE"
+        /// oraz dla oddanej książki W polu Wypozyczona status zostaje zmieniony na 0 (false);
+        /// Zmiany zostają zapisane, a panel użytkownika i panel zwrotu książek są ponownie renderowane.
+        /// </summary>
+
         private void ReturnBookDecisionButtonClick(object sender, RoutedEventArgs e)
         {
             if (BooksList.SelectedItem != null)
@@ -74,6 +100,11 @@ namespace BibliotekaWPF
                 ApplicationContext.GenerateUserPage();
             }
         }
+
+        /// <summary>
+        /// Metoda ReturnBookWindowClosing przy zamknięciu okna zwrotu książek w dowolny sposób
+        /// ustawia dla kontekstu aplikacji właściwość przechowującą instancję tego okna na null.
+        /// </summary>
 
         private void ReturnBookWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
         {
